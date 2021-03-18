b4d6b3ea16caf505950fc3a98727556a2e47fbdf
John Sanda <john.sanda@gmail.com>
2021-03-18 14:10:54 -0400

    fix protobuf package name

--- pkg/pb/medusa_grpc.pb.go
+++ pkg/pb/medusa_grpc.pb.go
@@ -11,7 +11,6 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // MedusaClient is the client API for Medusa service.
@@ -105,7 +104,7 @@ type UnsafeMedusaServer interface {
 }
 
 func RegisterMedusaServer(s grpc.ServiceRegistrar, srv MedusaServer) {
-	s.RegisterService(&Medusa_ServiceDesc, srv)
+	s.RegisterService(&_Medusa_serviceDesc, srv)
 }
 
 func _Medusa_Backup_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -180,10 +179,7 @@ func _Medusa_GetBackups_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
-// Medusa_ServiceDesc is the grpc.ServiceDesc for Medusa service.
-// It's only intended for direct use with grpc.RegisterService,
-// and not to be introspected or modified (even as a copy)
-var Medusa_ServiceDesc = grpc.ServiceDesc{
+var _Medusa_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "Medusa",
 	HandlerType: (*MedusaServer)(nil),
 	Methods: []grpc.MethodDesc{
