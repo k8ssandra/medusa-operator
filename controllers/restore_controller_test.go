f3e2e1513e31921f145571ca528108a9dd6b069e
John Sanda <john.sanda@gmail.com>
2021-06-17 09:25:45 -0400

    Replace Ginkgo (#47)
    * force upgrade racks to avoid rolling restart after scaling back up
    
    * updates from PR review
    
    * rewrite tests with testing instead of ginkgo (WIP)
    
    * fix import paths
    
    * clean up
    
    * add helper function
    
    * eliminate repeat args on verification function calls

---
+++ controllers/restore_controller_test.go
@@ -0,0 +1,167 @@
+package controllers
+
+import (
+	"context"
+	cassdcapi "github.com/k8ssandra/cass-operator/operator/pkg/apis/cassandra/v1beta1"
+	api "github.com/k8ssandra/medusa-operator/api/v1alpha1"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"testing"
+	"time"
+)
+
+func testInPlaceRestore(t *testing.T, ctx context.Context, namespace string) {
+	require := require.New(t)
+
+	restore := &api.CassandraRestore{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      "test-restore",
+		},
+		Spec: api.CassandraRestoreSpec{
+			Backup:   "test-backup",
+			Shutdown: true,
+			InPlace:  true,
+			CassandraDatacenter: api.CassandraDatacenterConfig{
+				Name:        TestCassandraDatacenterName,
+				ClusterName: "test-dc",
+			},
+		},
+	}
+
+	err := testClient.Create(ctx, restore)
+	require.NoError(err, "failed to create CassandraRestore")
+
+	dcKey := types.NamespacedName{Namespace: "default", Name: TestCassandraDatacenterName}
+
+	withDc := newWithDatacenter(t, ctx, dcKey)
+
+	t.Log("check that the datacenter is set to be stopped")
+	require.Eventually(withDc(func(dc *cassdcapi.CassandraDatacenter) bool {
+		return dc.Spec.Stopped == true
+	}), timeout, interval, "timed out waiting for CassandraDatacenter stopped flag to be set")
+
+	t.Log("delete datacenter pods to simulate shutdown")
+	err = testClient.DeleteAllOf(ctx, &corev1.Pod{}, client.InNamespace("default"), client.MatchingLabels{cassdcapi.DatacenterLabel: TestCassandraDatacenterName})
+	require.NoError(err, "failed to delete datacenter pods")
+
+	t.Log("check that the datacenter podTemplateSpec is updated")
+	require.Eventually(withDc(func(dc *cassdcapi.CassandraDatacenter) bool {
+		restoreContainer := findContainer(dc.Spec.PodTemplateSpec.Spec.InitContainers, "medusa-restore")
+		if restoreContainer == nil {
+			return false
+		}
+
+		envVar := findEnvVar(restoreContainer.Env, "BACKUP_NAME")
+		if envVar == nil || envVar.Value != "test-backup" {
+			return false
+		}
+
+		envVar = findEnvVar(restoreContainer.Env, "RESTORE_KEY")
+		return envVar != nil
+	}), timeout, interval, "timed out waiting for CassandraDatacenter PodTemplateSpec update")
+
+	t.Log("check datacenter force update racks")
+	require.Eventually(withDc(func(dc *cassdcapi.CassandraDatacenter) bool {
+		return len(dc.Spec.ForceUpgradeRacks) == 1 && dc.Spec.ForceUpgradeRacks[0] == "rack1"
+	}), timeout, interval)
+
+	t.Log("check restore status start time set")
+	require.Eventually(func() bool {
+		restore := &api.CassandraRestore{}
+		err := testClient.Get(ctx, types.NamespacedName{Namespace: "default", Name: "test-restore"}, restore)
+		if err != nil {
+			return false
+		}
+
+		return !restore.Status.StartTime.IsZero()
+	}, timeout, interval)
+
+	t.Log("check datacenter restarted")
+	require.Eventually(withDc(func(dc *cassdcapi.CassandraDatacenter) bool {
+		return !dc.Spec.Stopped
+	}), timeout, interval)
+
+	t.Log("set datacenter status to updated and ready")
+	dc := &cassdcapi.CassandraDatacenter{}
+	err = testClient.Get(ctx, dcKey, dc)
+	require.NoError(err, "failed to get datacenter")
+
+	restore = &api.CassandraRestore{}
+	err = testClient.Get(ctx, types.NamespacedName{Namespace: "default", Name: "test-restore"}, restore)
+	require.NoError(err, "failed to get restore object")
+
+	dc.SetCondition(cassdcapi.DatacenterCondition{
+		Type:               cassdcapi.DatacenterUpdating,
+		Status:             corev1.ConditionFalse,
+		LastTransitionTime: metav1.NewTime(restore.Status.StartTime.Time.Add(10 * time.Second)),
+	})
+	dc.SetCondition(cassdcapi.DatacenterCondition{
+		Type:               cassdcapi.DatacenterReady,
+		Status:             corev1.ConditionTrue,
+		LastTransitionTime: metav1.Now(),
+	})
+	dc.Status.CassandraOperatorProgress = cassdcapi.ProgressReady
+	dc.Status.SuperUserUpserted = metav1.Now()
+	dc.Status.UsersUpserted = metav1.Now()
+	dc.Status.LastServerNodeStarted = metav1.Now()
+	dc.Status.LastRollingRestart = metav1.Now()
+	dc.Status.NodeStatuses = cassdcapi.CassandraStatusMap{}
+	dc.Status.NodeReplacements = []string{}
+	dc.Status.QuietPeriod = metav1.Now()
+
+	err = testClient.Status().Update(ctx, dc)
+	require.NoError(err, "failed to set datacenter status to updated and ready")
+
+	t.Log("check restore status finish time set")
+	require.Eventually(func() bool {
+		restore := &api.CassandraRestore{}
+		err := testClient.Get(ctx, types.NamespacedName{Namespace: "default", Name: "test-restore"}, restore)
+		if err != nil {
+			return false
+		}
+
+		return restore.Status.FinishTime.After(restore.Status.StartTime.Time)
+	}, timeout, interval)
+}
+
+// newWithDatacenter is a function generator for withDatacenter that is bound to t, ctx, and key.
+func newWithDatacenter(t *testing.T, ctx context.Context, key types.NamespacedName) func(func(*cassdcapi.CassandraDatacenter) bool) func() bool {
+	return func(condition func(dc *cassdcapi.CassandraDatacenter) bool) func() bool {
+		return withDatacenter(t, ctx, key, condition)
+	}
+}
+
+// withDatacenter Fetches the CassandraDatacenter specified by key and then calls condition.
+func withDatacenter(t *testing.T, ctx context.Context, key types.NamespacedName, condition func(*cassdcapi.CassandraDatacenter) bool) func() bool {
+	return func() bool {
+		dc := &cassdcapi.CassandraDatacenter{}
+		if err := testClient.Get(ctx, key, dc); err == nil {
+			return condition(dc)
+		} else {
+			t.Logf("failed to get CassandraDatacenter: %s", err)
+			return false
+		}
+	}
+}
+
+func findContainer(containers []corev1.Container, name string) *corev1.Container {
+	for _, container := range containers {
+		if container.Name == name {
+			return &container
+		}
+	}
+	return nil
+}
+
+func findEnvVar(envVars []corev1.EnvVar, name string) *corev1.EnvVar {
+	for _, envVar := range envVars {
+		if envVar.Name == name {
+			return &envVar
+		}
+	}
+	return nil
+}
